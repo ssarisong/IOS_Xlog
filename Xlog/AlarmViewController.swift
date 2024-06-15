f15fa8e01a684ed808c3e4468716a8a965bc2921
ssarisong <thdtnwls2989@naver.com>
2024-06-16 01:47:58 +0900

    Add init UI

---
+++ Xlog/AlarmViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class AlarmViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Xlog/SeeViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class SeeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
